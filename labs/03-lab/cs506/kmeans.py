2b303c3f05fe3d39a722cca3632e44924f9987b6
BU-XY <90423290+BU-XY@users.noreply.github.com>
2022-09-29 16:28:14 -0400

    Add files via upload

--- labs/03-lab/cs506/kmeans.py
+++ labs/03-lab/cs506/kmeans.py
@@ -2,7 +2,10 @@ from collections import defaultdict
 from math import inf
 import random
 import csv
-
+import statistics
+import numpy as np
+import math
+from scipy.spatial import distance
 
 def get_centroid(points):
     """
@@ -11,7 +14,7 @@ def get_centroid(points):
     
     Returns a new point which is the center of all the points.
     """
-    raise NotImplementedError()
+    return [statistics.mean(g) for g in points]
 
 
 def get_centroids(dataset, assignments):
@@ -21,7 +24,11 @@ def get_centroids(dataset, assignments):
     Compute the centroid for each of the assigned groups.
     Return `k` centroids in a list
     """
-    raise NotImplementedError()
+    res = []
+    for ass in assignments:
+        a = np.array(dataset[i] for i in ass)
+        res.append([statistics.mean(g) for g in a])
+    return res
 
 
 def assign_points(data_points, centers):
@@ -44,15 +51,18 @@ def distance(a, b):
     """
     Returns the Euclidean distance between a and b
     """
-    raise NotImplementedError()
+    return math.dist(a,b)
 
 
 def distance_squared(a, b):
-    raise NotImplementedError()
+    return (math.dist(a,b))**2
 
 
 def cost_function(clustering):
-    raise NotImplementedError()
+    sum = 0
+    for point in clustering:
+        sum += distance_squared(point[0], point[1])
+    return sum
 
 
 def generate_k(dataset, k):
@@ -60,7 +70,7 @@ def generate_k(dataset, k):
     Given `data_set`, which is an array of arrays,
     return a random set of k points from the data_set
     """
-    raise NotImplementedError()
+    return random.sample(dataset, k)
 
 
 def generate_k_pp(dataset, k):
@@ -70,7 +80,33 @@ def generate_k_pp(dataset, k):
     where points are picked with a probability proportional
     to their distance as per kmeans pp
     """
-    raise NotImplementedError()
+    centers = []
+    X = np.array(X)
+    
+    # Sample the first point
+    initial_index = np.random.choice(range(X.shape[0]), )
+    centers.append(X[initial_index, :].tolist())
+    
+    print('max: ', np.max(np.sum((X - np.array(centers))**2)))
+    
+    # Loop and select the remaining points
+    for i in range(k - 1):
+        print(i)
+        dist = distance(X, np.array(centers))
+        
+        if i == 0:
+            pdf = dist/np.sum(dist)
+            centroid_new = X[np.random.choice(range(X.shape[0]), replace = False, p = pdf.flatten())]
+        else:
+            # Calculate the distance of each point from its nearest centroid
+            dist_min = np.min(dist, axis = 1)
+        pdf = dist_min/np.sum(dist_min)
+# Sample one point from the given distribution
+        centroid_new = X[np.random.choice(range(X.shape[0]), replace = False, p = pdf)]
+            
+        centers.append(centroid_new.tolist())
+        
+    return np.array(centers)
 
 
 def _do_lloyds_algo(dataset, k_points):
--- labs/03-lab/cs506/read.py
+++ labs/03-lab/cs506/read.py
@@ -1,6 +1,9 @@
+import numpy
+import csv
+
 def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    return numpy.array(list(csv.reader(open(csv_file_path, "rb"), delimiter=","))).astype("float")
